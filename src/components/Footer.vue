c10c8df64619ffd08e0223164ec5adcc06a71900
birnamwood <birnamwood19@gmail.com>
2019-07-26 02:11:42 +0900

    フッター追加

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+<header>
+    <nav>
+      <p>j.y 2019</P>
+    </nav>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
