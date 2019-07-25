e85071cbca44c5e63c19d8385c64e200c2711f4c
birnamwood <birnamwood19@gmail.com>
2019-07-26 01:43:29 +0900

    メインページ作成

---
+++ src/components/Mainview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mainview">
+    <p>メインビュー</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainview',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
