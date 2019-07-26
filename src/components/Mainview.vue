016ef45c2bb1db203a75606e444bf705b9b6a0f0
birnamwood <birnamwood19@gmail.com>
2019-07-26 14:01:00 +0900

    トップページ作成

--- src/components/Mainview.vue
+++ src/components/Mainview.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="mainview">
-    <p>メインビュー</p>
+    <p id="maintext">{{message}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'mainview',
+  data() {
+    return {
+      message: 'Welcome to My Portfolio.'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.mainview {
+  margin-bottom: auto;
+  background-image: url("../assets/main.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 
+#maintext {
+  color: white;
+  text-align: center;
+  margin: 50vh auto;
+  display: inline-block;
+  font-size: 36px;
+  width: 100%;
+  height: 100%;
+}
 </style>
