825c16ac8fda88598d62d108a14793036fdd6621
birnamwood <birnamwood19@gmail.com>
2019-07-29 14:53:22 +0900

    skillページ雛形作成

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,14 +1,63 @@
 <template>
-<div class="profile">
-      <p>スキル</p>
+  <div class="skill">
+      <div class="skill-box">
+          <div class="skill-title">
+            <i class="fas fa-tools" > Skill</i>
+          </div>
+          <table class="skill-table">
+            <tr>
+              <td>{{qualification}}</td>
+            </tr>
+          </table>
+      </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'skill',
+  data() {
+    return {
+      qualification: "資格：　基本情報",
+    }
+  }
+}
 </script>
+
+<style module>
+  @import "https://use.fontawesome.com/releases/v5.6.1/css/all.css";
+</style>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.skill {
+  height: 100%;
+  width: 100%;
+  padding-top: 70px;
+  padding-bottom: 45px;
+  margin: auto;
+}
+
+.skill-box {
+  margin: auto;
+  text-align: center;
+  background-color: lightblue;
+}
+.skill-title {
+  font-size: 60px;
+}
+
+.skill-table {
+  font-size:20px;
+  text-align: left;
+  margin: auto;
+  padding-bottom: 50px;
+}
+
+.skill-table td {
+  border-bottom: solid 1px;
+  border-collapse: collapse;
+  padding: 10px;
+}
 
 </style>
