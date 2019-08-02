e3e49db0dada7559ee5c11a8a8093f01d2e52c35
birnamwood <birnamwood19@gmail.com>
2019-08-02 13:30:25 +0900

    使用可能言語追加

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -37,6 +37,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .profile {
+
   width: 100%;
 }
 
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -6,13 +6,33 @@
           </div>
           <!--table-->
           <h2 class="qualification-title">資格</h2>
-          <table class="skill-table">
+          <table class="qualification-table">
             <ul class="qualification-list">
-              <li>{{qualification}}</li>
-              <li>{{qualification2}}</li>
-              <li>{{qualification3}}</li>
+              <li v-for="(qualification,index) in qualifications" :key="index">
+                {{qualification.name}}
+              </li>
             </ul>
           </table>
+
+          <h2 class="tool-title">技術（実務経験あり）</h2>
+          <table class="tool-table">
+            <ul class="tool-list">
+              <li v-for="(tool,index) in tools" :key="index">
+                {{tool.name}}
+              </li>
+            </ul>
+          </table>
+
+          <h2 class="tool-right-title">技術（勉強中）</h2>
+          <table class="tool-right-table">
+            <ul class="tool-right-list">
+              <li v-for="(toolright,index) in toolrights" :key="index">
+                {{toolright.name}}
+              </li>
+            </ul>
+          </table>
+
+
       </div>
   </div>
 </template>
@@ -22,9 +42,28 @@ export default {
   name: 'skill',
   data() {
     return {
-      qualification: "基本情報処理技術者",
-      qualification2: "3級知的財産管理技能士",
-      qualification3: "日商簿記2級",
+      qualifications: [
+        { name: "基本情報処理技術者"},
+        { name: "3級知的財産管理技能士"},
+        { name: "日商簿記2級"},
+      ],
+      tools: [
+        { name: "RPG Ⅲ, Ⅳ"},
+        { name: "VB6"},
+        { name: "VB.NET"},
+        { name: "C#.NET"},
+        { name: "Windows"},
+        { name: "CENTOS 7"},
+        { name: "VMware/vSphere"},
+        { name: "HYPER-V"},
+      ],
+      toolrights: [
+        { name: "HTML/CSS"},
+        { name: "javaScript/Vue.js"},
+        { name: "Python/Django"},
+        { name: "Ruby/Rails"},
+        { name: "ruby/Rails"},
+      ],
     }
   }
 }
@@ -37,7 +76,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .skill {
-  height: 500px;
   width: 100%;
   padding-bottom: 50px;
 }
@@ -48,27 +86,78 @@ export default {
   height: 100%;
   padding-top: 100px;
   padding-bottom: 100px;
-  background-color: lightblue;
 }
 .skill-title {
   font-size: 60px;
 }
 
-.skill-table {
+.qualification-title {
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.qualification-table {
+  width: 90%;
   font-size:20px;
   text-align: left;
   margin: auto;
   border-radius: 5px;
-  background-color: white;
   box-shadow: 0px 0px 10px 0px gray;
 }
 .qualification-list {
-  list-style: none;
+  width: 50%;
+  margin: auto;
   padding: 10px;
 }
 .qualification-list li {
-  padding: 5px;
+  padding: 10px;
 }
 
+.tool-title {
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+.tool-table {
+  width: 90%;
+  font-size:20px;
+  text-align: left;
+  margin: auto;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px 0px gray;
+}
+.tool-list {
+  margin: auto;
+}
+.tool-list li {
+  width: 45%;
+  margin: 10px;
+  float: left;
+}
+
+.tool-right-title {
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+.tool-right-table {
+  width: 90%;
+  font-size:20px;
+  text-align: left;
+  margin: auto;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px 0px gray;
+}
+.tool-right-list {
+  margin: auto;
+}
+.tool-right-list li {
+  width: 45%;
+  margin: 10px;
+  float: left;
+}
+
+@media (max-width: 640px) {
+  .qualification-list {
+    width: 60%;
+}
+}
 
 </style>
