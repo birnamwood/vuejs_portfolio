34dc5591d7ef343cc5760b2302c4854130eb31f8
birnamwood <birnamwood19@gmail.com>
2019-08-06 11:49:55 +0900

    自己紹介追加

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -10,6 +10,7 @@
               <tr><td>年齢</td><td>{{age}}</td></tr>
               <tr><td>住所</td><td>{{address}}</td></tr>
               <tr><td>趣味</td><td>{{hobby}}</td></tr>
+              <tr><td>自己紹介</td><td>{{introduction}}<br>{{introduction2}}</td></tr>
             </tbody>
           </table>
       </div>
@@ -25,6 +26,8 @@ export default {
       age: "29歳",
       address: "京都府長岡京市",
       hobby: "バイク、旅行",
+      introduction: "4年制大学の法学部を卒業後、医療系の会社で５年間社内SEをしていました。",
+      introduction2: "IBMメインフレームでの基幹システム開発・保守、ActiveDirectoryなどのサーバー保守及び社内ネットワーク管理と一通りの経験があります。"
     }
   }
 }
@@ -66,6 +69,7 @@ export default {
   border-top: 1px solid lightgray;
   border-bottom: 1px solid lightgray;
   padding: 15px;
+  min-width: 120px;
 }
 
 .profile-table tbody tr:nth-of-type(2n) {
